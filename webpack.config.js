8b17bec6a4a184330e98136177e3f3592ed8ff46
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-18 23:25:54 -0300

    feat: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,54 @@
-const path = require('path');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',  // Entry point of your library
-  output: {
-    filename: 'galadriel.js', // Output file name
-    path: path.resolve(__dirname, 'dist'), // Output directory
-    library: 'Galadriel', // Library name accessible in the browser
-    libraryTarget: 'umd', // Universal Module Definition (UMD)
-    umdNamedDefine: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Use Babel for transpiling
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+    entry: "./src/index.ts", // Entry point of your library
+    output: {
+        filename: "index.js", // Output file name
+        path: path.resolve(__dirname, "dist"), // Output directory
+        library: "Galadriel", // Library name accessible in the browser
+        libraryTarget: "umd", // Universal Module Definition (UMD)
+        umdNamedDefine: true,
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader", // Use Babel for transpiling
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
+    mode: "production", // or 'development' for non-minified output
+    devtool: "source-map", // Generate source maps
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
         },
-      },
+        compress: true, // Enable gzip compression
+        port: 8080, // Port number
+        hot: true, // Enable hot module replacement
+    },
+    watchOptions: {
+        ignored: ["node_modules", "build", "dist/**"],
+    },
+    plugins: [
+        // Copy everything from src to dist, including HTML files
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "src",
+                    to: "",
+                },
+            ],
+        }),
     ],
-  },
-  mode: 'production', // or 'development' for non-minified output
-  devtool: 'source-map', // Generate source maps
 };
