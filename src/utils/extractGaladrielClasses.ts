393c5c8e940f5f45c827857e40516d15da359564
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-18 23:28:00 -0300

    feat: utility

---
+++ src/utils/extractGaladrielClasses.ts
@@ -0,0 +1,5 @@
+import { ExtractGaladrielClassesType } from "../../types/coreTypes";
+
+export const extractGaladrielClasses: ExtractGaladrielClassesType = (classes) => {
+    return Object.keys(classes).map(key => key.replace(".", ""))
+}
---
+++ src/utils/genIntellisenseFile.ts
@@ -0,0 +1,20 @@
+import { writeFileSync } from "fs";
+import { dynamicStylesObject } from "./dynamicStylesObject";
+import { genGaladrielTypes } from "./genGaladrielTypes";
+
+const staticStyles = dynamicStylesObject();
+const objectsMerges = { ...staticStyles };
+const intelliSenseStr = genGaladrielTypes(objectsMerges);
+
+const genIntellisenseFile = () => {
+    try {
+        writeFileSync("./types/galadrielIntelliSense.ts", intelliSenseStr, "utf-8");
+        console.log("galadrielIntelliSense.d.ts generated successfully!");
+    } catch (error: any) {
+        console.log(
+            `Failed to generate galadrielIntelliSense.d.ts: ${error.message}`
+        );
+    }
+};
+
+genIntellisenseFile();
