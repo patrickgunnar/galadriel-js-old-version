cc609a100dba451dccd79333cce290706aa86428
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-21 13:22:29 -0300

    refactor: moved files

--- tools/utils/extractGaladrielClasses.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractGaladrielClasses = void 0;
-var extractGaladrielClasses = function (classes) {
-    return Object.keys(classes).map(function (key) { return key.replace(".", ""); });
-};
-exports.extractGaladrielClasses = extractGaladrielClasses;
