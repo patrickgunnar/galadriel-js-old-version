c2e974b5b0ebeac01d13bc8a7ce1c1b2f0202d2d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-21 21:29:49 -0300

    feat: improved code to get nested objects

--- plugins/script/extractJSObjects.js
+++ plugins/script/extractJSObjects.js
@@ -1,17 +1,27 @@
-function extractJSObjects(node) {
+function extractJSObjects(node, processedObjects = new Set()) {
     const extractedObjects = [];
+
+    // Generate a unique hash for the current object node
+    const nodeHash = JSON.stringify(node);
+
+    // Check if this object has been processed before
+    if (processedObjects.has(nodeHash)) {
+        return extractedObjects;
+    }
+
+    // Add the hash of this object to the processed set
+    processedObjects.add(nodeHash);
 
     if (node.type === "ObjectExpression") {
         const obj = {};
 
         for (const property of node.properties) {
-            if (
-                property.type === "Property" &&
-                property.value.type === "Literal"
-            ) {
-                const key = property.key.name || property.key.value;
-                const value = property.value.value;
+            const key = property.key.name || property.key.value;
 
+            if (property.value.type === "ObjectExpression") {
+                obj[key] = extractJSObjects(property.value, processedObjects);
+            } else {
+                const value = property.value.value;
                 obj[key] = value;
             }
         }
@@ -20,8 +30,8 @@ function extractJSObjects(node) {
     }
 
     for (const key in node) {
-        if (node[key] && typeof node[key] === "object") {
-            const childObjects = extractJSObjects(node[key]); // Recursive call for object properties
+        if (node[key] && typeof node[key] === "object" && key !== "parent") {
+            const childObjects = extractJSObjects(node[key], processedObjects); // Recursive call for object properties
             extractedObjects.push(...childObjects);
         }
     }
