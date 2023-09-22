d7799753a4fec9598c276b799425cf2530a84ee6
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-22 12:56:07 -0300

    feat: types prestart

--- scripts/prestart/compileToJavaScript.js
+++ scripts/prestart/compileToJavaScript.js
@@ -1,15 +1,14 @@
 const { exec } = require("child_process");
 
 const compileToJavaScript = [
-    "tools/coreStaticStyles.ts",
-    "scripts/genStaticClasses.ts",
+    "scripts/prestart/intellisenseTypes.ts",
     "scripts/dynamicStylesObject.ts",
     "scripts/genGaladrielTypes.ts",
     "scripts/extractGaladrielClasses.ts",
 ];
 
 compileToJavaScript.forEach((pattern) => {
-    exec(`tsc ${pattern} --outDir scripts/builds`, (error, stdout, stderr) => {
+    exec(`tsc ${pattern} --outDir builds`, (error, stdout, stderr) => {
         if (error) {
             console.error(
                 `Error compiling files matching pattern ${pattern}: ${stderr}`
