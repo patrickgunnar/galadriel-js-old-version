4b5aea5759c6c1e516f752f64bc82c08c11692fb
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-23 12:49:02 -0300

    refactor: added if condition

--- plugins/MystiCSSor/scripts/computeCSSFromObject.ts
+++ plugins/MystiCSSor/scripts/computeCSSFromObject.ts
@@ -1,4 +1,4 @@
-import { coreStaticStyles } from "../../../EssentiaHub./coreStaticStyles";
+import { coreStaticStyles } from "../../../EssentiaHub/coreStaticStyles";
 import { ExtractGaladrielCSSClassesType } from "../../../types/coreTypes";
 
 const generateUtilityClasses: ExtractGaladrielCSSClassesType = (classes) =>
@@ -7,43 +7,45 @@ const generateUtilityClasses: ExtractGaladrielCSSClassesType = (classes) =>
 const getStaticStyles = (__key: string, __value: any): string | null => {
     const __staticHandler = coreStaticStyles[__key];
 
-    try {
-        const __propertiesHandler = __staticHandler({
-            extractGaladrielClasses: generateUtilityClasses,
-        });
+    if (__staticHandler) {
+        try {
+            const __propertiesHandler = __staticHandler({
+                extractGaladrielClasses: generateUtilityClasses,
+            });
 
-        const __selector = `.${__value}`;
-        const __propertiesValues = __propertiesHandler[__selector];
+            const __selector = `.${__value}`;
+            const __propertiesValues = __propertiesHandler[__selector];
 
-        if (__propertiesValues) {
-            try {
-                const __pseudClasses: string[] = [];
-                const __classesString = Object.entries(__propertiesValues)
-                    .map(([__property, __asset]) => {
-                        if (__property.includes("&")) {
-                            __pseudClasses.push(
-                                `${__property.replace(
-                                    "&",
-                                    __selector
-                                )} ${__asset}`
-                            );
+            if (__propertiesValues) {
+                try {
+                    const __pseudClasses: string[] = [];
+                    const __classesString = Object.entries(__propertiesValues)
+                        .map(([__property, __asset]) => {
+                            if (__property.includes("&")) {
+                                __pseudClasses.push(
+                                    `${__property.replace(
+                                        "&",
+                                        __selector
+                                    )} ${__asset}`
+                                );
 
-                            return;
-                        }
+                                return;
+                            }
 
-                        return `${__property}: ${__asset};`;
-                    })
-                    .join(" ");
+                            return `${__property}: ${__asset};`;
+                        })
+                        .join(" ");
 
-                return `${__selector} { ${__classesString} } ${__pseudClasses.join(
-                    " "
-                )}`;
-            } catch (error: any) {
-                console.error("An error occurred:", error);
+                    return `${__selector} { ${__classesString} } ${__pseudClasses.join(
+                        " "
+                    )}`;
+                } catch (error: any) {
+                    console.error("An error occurred:", error);
+                }
             }
+        } catch (error: any) {
+            console.error("An error occurred:", error);
         }
-    } catch (error: any) {
-        console.error("An error occurred:", error);
     }
 
     return null;
