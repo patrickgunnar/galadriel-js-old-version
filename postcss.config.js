0b5b551e0712ad4928aefce6c45f3b4cd60ab59a
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-23 18:54:44 -0300

    refactor: removed class harvest plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-const {
-    classHarvestPlugin,
-} = require("./dist/plugins/MystiCSSor/classHarvestPlugin");
-
 const postcssAutoprefixer = require("autoprefixer");
 const postcssCssnano = require("cssnano");
 //const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
-    plugins: [
-        classHarvestPlugin(),
-        postcssAutoprefixer(),
-        postcssCssnano(),
-    ],
+    plugins: [postcssAutoprefixer(), postcssCssnano()],
 };
