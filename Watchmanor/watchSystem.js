762ac73fd2c63f39a5077876512626dcbbb6af27
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-23 18:53:51 -0300

    feat: added class harvest plugin

--- Watchmanor/watchSystem.js
+++ Watchmanor/watchSystem.js
@@ -41,6 +41,22 @@ const signalWebpackDevServer = () => {
     }
 };
 
+const classHarvestPlugin = () => {
+    exec(
+        "node dist/plugins/MystiCSSor/classHarvestPlugin.js",
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log(error);
+                console.error(
+                    `Encountered an error during the classes harvest process: ${stderr}.`
+                );
+            } else {
+                console.log("Successfully harvested the CSS classes.");
+            }
+        }
+    );
+};
+
 const compileTypeScript = () => {
     console.log("Compiling with 'tsc --incremental'.");
 
@@ -55,8 +71,8 @@ const compileTypeScript = () => {
                 "Successfully completed the TypeScript code compilation process."
             );
 
-            // Signal the webpack server that new code was compiled
-            ///signalWebpackDevServer();
+            // call harvest classes plugin
+            classHarvestPlugin();
         }
     });
 };
