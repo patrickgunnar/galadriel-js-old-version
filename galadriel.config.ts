99adc3d44575b02a1968994a1f1e2d5e8d176671
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-23 18:53:26 -0300

    refactor: added dummy content

--- galadriel.config.ts
+++ galadriel.config.ts
@@ -1,3 +1,8 @@
 module.exports = {
     exclude: ["node_modules", "dist", "plugins", "scripts", "test", "toolkit", "core", "types"],
+    display: {
+        height: "10px",
+        width: "20px",
+        black: "#000000"
+    }
 };
