38ce3691fae38878b4f5408bd3f5b445f1b915b8
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-25 13:11:45 -0300

    feat: added tests property

--- galadriel.config.ts
+++ galadriel.config.ts
@@ -1,5 +1,9 @@
 module.exports = {
     exclude: ["node_modules", "dist", "PatterniaHub", "plugins", "StyleCrafters", "test", "types", "Watchmanor"],
+    /*tests: {
+        test: true,
+        testPath: ["test/mocks/index.js", "test/mocks/index.ts", "test/mocks/index.html"]
+    },*/
     display: {
         height: "10px",
         width: "20px",
