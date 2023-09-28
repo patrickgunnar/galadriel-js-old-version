5706f5e2e97a599b9c8a69d0525a4abc6e67b56e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-28 12:05:39 -0300

    feat: added error handler

--- plugins/MystiCSSor/scripts/extractKeysValueFromObjectExpression.ts
+++ plugins/MystiCSSor/scripts/extractKeysValueFromObjectExpression.ts
@@ -3,40 +3,50 @@ import { Node } from "@babel/types";
 type ParsedValue = string | Record<string, any> | null;
 
 const extractNestedObjs = (node: Node): ParsedValue => {
-    if ((node as any).type.includes("Literal")) {
-        return (node as any).value;
-    } else if (node.type === "ObjectExpression" && node.properties) {
-        const nestedKeyValues: Record<string, any>[] = node.properties.map(
-            (property) => ({
-                [(property as any).key.name]: extractNestedObjs(
-                    (property as any).value
-                ),
-            })
-        );
+    try {
+        if ((node as any).type.includes("Literal")) {
+            return (node as any).value;
+        } else if (node.type === "ObjectExpression" && node.properties) {
+            const nestedKeyValues: Record<string, any>[] = node.properties.map(
+                (property) => ({
+                    [(property as any).key.name]: extractNestedObjs(
+                        (property as any).value
+                    ),
+                })
+            );
 
-        return Object.assign({}, ...nestedKeyValues);
+            return Object.assign({}, ...nestedKeyValues);
+        }
+    } catch (error: any) {
+        console.error("An error occurred:", error);
     }
 
     return null; // Handle unknown node types
 };
 
 const extractKeysValueFromObjectExpression = (node: Node) => {
-    if (node.type === "ObjectExpression" && node.properties) {
-        const properties = node.properties;
+    try {
+        if (node.type === "ObjectExpression" && node.properties) {
+            const properties = node.properties;
 
-        return properties.reduce((acc: Record<string, any>, property) => {
-            if (property.type === "ObjectProperty" && property.key) {
-                const key = (property.key as any).name;
+            return properties.reduce((acc: Record<string, any>, property) => {
+                if (property.type === "ObjectProperty" && property.key) {
+                    const key = (property.key as any).name;
 
-                if (key) {
-                    const value = extractNestedObjs(property.value);
+                    if (key) {
+                        const value = extractNestedObjs(property.value);
 
-                    acc[key] = value;
+                        acc[key] = value;
+                    }
                 }
-            }
 
-            return acc;
-        }, {});
+                return acc;
+            }, {});
+        }
+    } catch (error: any) {
+        console.error("An error occurred:", error);
+
+        return {};
     }
 };
 
