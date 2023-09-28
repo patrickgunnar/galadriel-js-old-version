55e5f30bd8d1a8fbc8187dde3d137d7a4c30e1e4
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-28 11:58:47 -0300

    feat: added error handler

--- plugins/MystiCSSor/galadrielHarvestPlugin.ts
+++ plugins/MystiCSSor/galadrielHarvestPlugin.ts
@@ -11,47 +11,54 @@ export default function (): PluginObj {
     return {
         visitor: {
             ObjectExpression(path: NodePath) {
-                const node = path.node as Node;
-                const stringifiedNode = JSON.stringify(node);
-                const jointCompletedNode = completedNode.join(" ");
+                try {
+                    const node = path.node as Node;
+                    const stringifiedNode = JSON.stringify(node);
+                    const jointCompletedNode = completedNode.join(" ");
 
-                if (node && !jointCompletedNode.includes(stringifiedNode)) {
-                    completedNode.push(stringifiedNode);
+                    if (node && !jointCompletedNode.includes(stringifiedNode)) {
+                        completedNode.push(stringifiedNode);
 
-                    const objects = extractKeysValueFromObjectExpression(node);
-                    const stringifiedObjs = JSON.stringify(objects);
-                    const jointObjs = completedObjs.join(" ");
+                        const objects =
+                            extractKeysValueFromObjectExpression(node);
+                        const stringifiedObjs = JSON.stringify(objects);
+                        const jointObjs = completedObjs.join(" ");
 
-                    if (objects && !jointObjs.includes(stringifiedObjs)) {
-                        completedObjs.push(stringifiedObjs);
+                        if (objects && !jointObjs.includes(stringifiedObjs)) {
+                            completedObjs.push(stringifiedObjs);
 
-                        for (const [key, value] of Object.entries(objects)) {
-                            if (value && typeof value === "object") {
-                                for (const [
-                                    nestedKey,
-                                    nestedValue,
-                                ] of Object.entries(value)) {
-                                    parseNestedObjClasses(
-                                        key,
+                            for (const [key, value] of Object.entries(
+                                objects
+                            )) {
+                                if (value && typeof value === "object") {
+                                    for (const [
                                         nestedKey,
-                                        nestedValue
+                                        nestedValue,
+                                    ] of Object.entries(value)) {
+                                        parseNestedObjClasses(
+                                            key,
+                                            nestedKey,
+                                            nestedValue
+                                        );
+                                    }
+                                } else {
+                                    const ruleString = computeCSSFromObject(
+                                        key,
+                                        value
                                     );
-                                }
-                            } else {
-                                const ruleString = computeCSSFromObject(
-                                    key,
-                                    value
-                                );
 
-                                if (
-                                    ruleString &&
-                                    !styleRules.includes(ruleString)
-                                ) {
-                                    styleRules.push(ruleString);
+                                    if (
+                                        ruleString &&
+                                        !styleRules.includes(ruleString)
+                                    ) {
+                                        styleRules.push(ruleString);
+                                    }
                                 }
                             }
                         }
                     }
+                } catch (error: any) {
+                    console.error("An error occurred:", error);
                 }
             },
         },
