0b327ff44cb49390c935bdb56bc62c6f6a8fdbf2
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-28 19:41:27 -0300

    refactor: improvements

--- plugins/TypeWeaver/typeForgePlugin.ts
+++ plugins/TypeWeaver/typeForgePlugin.ts
@@ -1,21 +1,19 @@
 import * as fs from "fs";
 import * as path from "path";
 import { dynamicObjectManager } from "./scripts/dynamicObjectManager";
-import { typeCraftsman } from "./scripts/typeCraftsman";
 
 const typeForgePlugin = () => {
     const objectsData = dynamicObjectManager();
-    const typesString = typeCraftsman(objectsData);
 
     try {
         fs.writeFileSync(
             path.join(__dirname, "..", "..", "..", "types", "typeManifest.ts"),
-            typesString
+            `export type CreateClassesType = { ${objectsData} }`
         );
 
         fs.writeFileSync(
             path.join(__dirname, "..", "..", "types", "typeManifest.d.ts"),
-            typesString
+            `export type CreateClassesType = { ${objectsData} }`
         );
     } catch (error: any) {
         console.error("An error occurred:", error);
