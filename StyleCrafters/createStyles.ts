afa12008813603acb36d9cb72928189ccedbd0d4
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-28 11:55:38 -0300

    feat: added dynamic handler

--- StyleCrafters/createStyles.ts
+++ StyleCrafters/createStyles.ts
@@ -1,4 +1,7 @@
 //@ts-ignore
+import { coreDynamicProperties } from "../PatterniaHub/coreDynamicProperties";
+import { coreStaticStyles } from "../PatterniaHub/coreStaticStyles";
+import { ExtractGaladrielCSSClassesType } from "../types/coreTypes";
 import { CreateClassesType } from "../types/typeManifest";
 
 interface CallbackType {
@@ -9,8 +12,33 @@ interface CreateStylesType {
     (callback: CallbackType): string;
 }
 
+const getClasses: ExtractGaladrielCSSClassesType = (cls) => cls;
+
 const createStyles: CreateStylesType = (callback): string => {
-    return Object.values(callback() ?? {}).join(" ") || "";
+    return Object.entries(callback()).reduce((acc, [key, value]) => {
+        const staticUtilities = coreStaticStyles[key]?.({
+            extractGaladrielClasses: getClasses,
+        });
+
+        if (staticUtilities) {
+            const styles = staticUtilities[`.${value}`] ?? null;
+
+            if (styles) {
+                return acc + (acc.length > 0 ? " " : "") + value;
+            }
+        } else {
+            const property = coreDynamicProperties[key] ?? null;
+
+            if (property && value && typeof value === "string") {
+                const sanitizedValue = value.replace(/[^a-zA-Z0-9]/g, "");
+                const cssRule = `galadriel-${property}__${sanitizedValue}`;
+
+                return acc + (acc.length > 0 ? " " : "") + cssRule;
+            }
+        }
+
+        return acc;
+    }, "");
 };
 
 export { createStyles };
