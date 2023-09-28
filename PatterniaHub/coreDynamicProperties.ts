ea6e3349507fb6acc77026fe9dcf350b5a80cf82
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-28 11:55:22 -0300

    feat: dynamic core

---
+++ PatterniaHub/coreDynamicProperties.ts
@@ -0,0 +1,6 @@
+export const coreDynamicProperties: Record<string, string> = {
+    "backgroundColor": "background-color",
+    "display": "display",
+    "height": "height",
+    "width": "width",
+};
