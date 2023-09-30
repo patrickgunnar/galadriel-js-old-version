ca8cd2a2470c0bd59ef7b32a0756e239b8f8f16e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-30 14:33:28 -0300

    feat: implement pseudo-classes handler

--- plugins/MystiCSSor/scripts/parseNestedObjClasses.ts
+++ plugins/MystiCSSor/scripts/parseNestedObjClasses.ts
@@ -1,11 +1,105 @@
-const parseNestedObjClasses = (
+import { coreDynamicProperties } from "../../../PatterniaHub/coreDynamicProperties";
+import { coreStaticStyles } from "../../../PatterniaHub/coreStaticStyles";
+import { ExtractGaladrielCSSClassesType } from "../../../types/coreTypes";
+
+const getClasses: ExtractGaladrielCSSClassesType = (classes) => classes;
+
+const getStaticStyles = (
     key: string,
-    nestedObjKey: string,
-    nestedObjValue: any
-) => {
-    // THIS FUNCTION MUST CALL extractClasses PASSING THE nestedKey AND THE nestedValue
-    // AND RETURN THE RETURN OF extractClasses
-    console.log(key, " --> ", nestedObjKey, nestedObjValue);
+    value: any
+): Record<string, any> | null => {
+    try {
+        const staticHandler = coreStaticStyles[key];
+
+        if (staticHandler) {
+            const propertiesHandler = staticHandler({
+                extractGaladrielClasses: getClasses,
+            });
+
+            const selector = `.${value}`;
+            const propertiesValues = propertiesHandler[selector];
+
+            if (propertiesValues) {
+                return propertiesValues;
+            }
+        }
+    } catch (error: any) {
+        console.error("An error occurred:", error);
+    }
+
+    return null;
+};
+
+const getDynamicStyles = (key: string): string | null => {
+    try {
+        return coreDynamicProperties[key];
+    } catch (error: any) {
+        console.error("An error occurred:", error);
+    }
+
+    return null;
+};
+
+const parseNestedObjClasses = (objKey: string, objValue: any) => {
+    const styleRules: Record<string, any>[] = [];
+    const stylesValues: string[] = [];
+
+    for (const [nestedKey, nestedValue] of Object.entries(objValue)) {
+        const staticRules = getStaticStyles(nestedKey, nestedValue);
+
+        stylesValues.push(nestedKey);
+        stylesValues.push(nestedValue as string);
+
+        if (!staticRules) {
+            const dynamicRules = getDynamicStyles(nestedKey);
+
+            if (dynamicRules) {
+                styleRules.push({
+                    [dynamicRules]: nestedValue,
+                });
+            }
+        } else {
+            styleRules.push(staticRules);
+        }
+    }
+
+    if (styleRules) {
+        const sanitizedSpecialChars = /[^a-zA-Z0-9]/g;
+        const sanitizedCamelCase = /([a-z])([A-Z])/g;
+
+        const styles = styleRules.reduce((acc, obj) => {
+            const [[key, value]] = Object.entries(obj);
+
+            if (key && value) {
+                console.log(key, " --> ", value);
+
+                return (acc += `${key}: ${value}; `);
+            }
+
+            return acc;
+        }, "");
+
+        const pseudoClass = getDynamicStyles(objKey);
+
+        if (pseudoClass) {
+            const valuesName = stylesValues
+                .join("-")
+                .replace(/[aeiou]/gi, "")
+                .replace(sanitizedSpecialChars, "_")
+                .replace(/_+/g, "_");
+
+            const className = `.${objKey.replace(
+                sanitizedCamelCase,
+                "$1-$2"
+            )}__${valuesName}`.toLowerCase();
+
+            const classNameWithPseudo = pseudoClass.replace("&", className);
+
+            return `${classNameWithPseudo} { ${styles} }`;
+        }
+    }
+
+    return null;
 };
 
 export { parseNestedObjClasses };
