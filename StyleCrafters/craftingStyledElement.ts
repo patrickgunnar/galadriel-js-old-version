4596fa153febeb8151377c7df6c23a24fe9ceffa
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-30 10:01:08 -0300

    refactor: split same code in different file

--- StyleCrafters/craftingStyledElement.ts
+++ StyleCrafters/craftingStyledElement.ts
@@ -1,6 +1,7 @@
 //@ts-ignore
 import { HTMLElementTagName } from "../types/coreTypes";
 import { CraftClassesType } from "../types/typeManifest";
+import { genClassNames } from "./utils/genClassNames";
 
 interface CallbackType {
     (): CraftClassesType;
@@ -15,55 +16,10 @@ const craftingStyledElement: CraftStylesType = (
     callback
 ): HTMLElement => {
     const createdElement = document.createElement(element);
-    const testRegex = /^__\w+(-\w+)*$/;
-    const sanitizedSpecialChars = /[^a-zA-Z0-9]/g;
-    const sanitizedCamelCase = /([a-z])([A-Z])/g;
+    const classNames = genClassNames(callback());
 
-    try {
-        const classNames = Object.entries(callback()).reduce(
-            (acc, [key, value]) => {
-                const isMatchingPattern = testRegex.test(value);
-
-                if (isMatchingPattern) {
-                    return acc + (acc.length > 0 ? " " : "") + value;
-                } else if (typeof value === "object") {
-                    const nestedClass = Object.entries(value)
-                        .filter(([nestedKey, nestedValue]) => {
-                            if (
-                                typeof nestedValue !== "object" &&
-                                nestedValue !== null &&
-                                nestedValue !== undefined
-                            ) {
-                                return `${nestedKey}-${nestedValue}`;
-                            }
-                        })
-                        .join("-")
-                        .replace(/[aeiou]/gi, "")
-                        .replace(sanitizedSpecialChars, "_")
-                        .replace(/_+/g, "_");
-
-                    return (
-                        acc +
-                        (acc.length > 0 ? " " : "") +
-                        `${key.replace(
-                            sanitizedCamelCase,
-                            "$1-$2"
-                        )}__${nestedClass}`.toLowerCase()
-                    );
-                } else {
-                    const sanitizedValue = value.replace(sanitizedSpecialChars, "");
-                    const sanitizedKey = key.replace(sanitizedCamelCase, "$1-$2");
-                    const cssRule = `galadriel-${sanitizedKey.toLowerCase()}__${sanitizedValue}`;
-
-                    return acc + (acc.length > 0 ? " " : "") + cssRule;
-                }
-            },
-            ""
-        );
-
+    if (classNames) {
         createdElement.className = classNames;
-    } catch (error: any) {
-        console.error("An error occurred:", error);
     }
 
     return createdElement;
