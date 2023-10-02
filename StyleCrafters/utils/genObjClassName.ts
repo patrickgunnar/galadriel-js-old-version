15bff9b6d3693d472cb68653fb771af1c28abd8b
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-02 08:56:01 -0300

    refactor: added extra sanitizer

--- StyleCrafters/utils/genObjClassName.ts
+++ StyleCrafters/utils/genObjClassName.ts
@@ -1,20 +1,11 @@
 const genObjClassName = (key: string, value: string) => {
-    const customClass: string[] = [];
     const sanitizedSpecialChars = /[^a-zA-Z0-9]/g;
     const sanitizedCamelCase = /([a-z])([A-Z])/g;
 
     const nestedClass = Object.entries(value)
         .map(([nestedKey, nestedValue]) => {
-            if (typeof nestedValue === "string" && !nestedValue.includes("$")) {
+            if (typeof nestedValue === "string") {
                 return `${nestedKey}-${nestedValue}`;
-            }
-
-            if (nestedValue) {
-                const sanitizedNestedValue = (nestedValue as string).replace(
-                    "$",
-                    ""
-                );
-                customClass.push(`${sanitizedNestedValue}_${key}`);
             }
 
             return "";
@@ -22,14 +13,11 @@ const genObjClassName = (key: string, value: string) => {
         .filter(Boolean)
         .join("-")
         .replace(/[aeiou]/gi, "")
+        .replace(sanitizedCamelCase, "$1-$2")
         .replace(sanitizedSpecialChars, "_")
         .replace(/_+/g, "_");
 
-    const customStr = customClass
-        .filter((str) => str.trim().replace(/\s/g, "").length > 0)
-        .join(" ");
-
-    return `${key.replace(sanitizedCamelCase, "$1-$2").toLowerCase()}__${nestedClass}` + (customStr ? ` ${customStr}` : "");
+    return `${key.replace(sanitizedCamelCase, "$1-$2")}__${nestedClass}`.toLowerCase();
 };
 
 export { genObjClassName };
