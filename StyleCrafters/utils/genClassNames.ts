bed3892961d215e2d49d8ccf5c1a1f592e0e20f7
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-03 09:55:39 -0300

    refactor: improved logic

--- StyleCrafters/utils/genClassNames.ts
+++ StyleCrafters/utils/genClassNames.ts
@@ -1,32 +1,12 @@
-import { genObjClassName } from "./genObjClassName";
+const genClassNames = (classes: Record<string, any>): string => {
+    const classNames = Object.values(classes).map((cls: string) => {
+        if (typeof cls === "string") return cls;
+        else if (typeof cls === "object") {
+            return Object.values(cls)[0];
+        }
+    });
 
-const genClassNames = (rulesObj: Record<string, any>): string => {
-    const testRegex = /^\$\w+(-\w+)*$/;
-    const sanitizedSpecialChars = /[^a-zA-Z0-9]/g;
-    const sanitizedCamelCase = /([a-z])([A-Z])/g;
-
-    try {
-        return Object.entries(rulesObj)
-            .reduce((acc, [key, value]) => {
-                if (testRegex.test(value)) {
-                    return acc + `${acc ? " " : ""}${value.replace("$", "")}`;
-                }
-
-                if (typeof value === "object") {
-                    return acc + `${acc ? " " : ""}${genObjClassName(key, value)}`;
-                }
-
-                const sanitizedValue = value.replace(sanitizedSpecialChars, "");
-                const sanitizedKey = key.replace(sanitizedCamelCase, "$1-$2");
-                const cssRule = `galadriel-${sanitizedKey.toLowerCase()}__${sanitizedValue}`;
-
-                return acc + `${acc ? " " : ""}${cssRule}`;
-            }, "")
-            .trim();
-    } catch (error: any) {
-        console.error("An error occurred:", error);
-        return "";
-    }
+    return classNames.join(" ");
 };
 
 export { genClassNames };
