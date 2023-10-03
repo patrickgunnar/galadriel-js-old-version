282b6613e299c185eac6e261de585e4b2f9f655a
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-03 00:32:55 -0300

    refactor: improved logic

---
+++ plugins/MystiCSSor/scripts/getNestedStyles.ts
@@ -0,0 +1,100 @@
+import { Node } from "@babel/types";
+import { coreDynamicProperties } from "../../../PatterniaHub/coreDynamicProperties";
+import { coreStaticStyles } from "../../../PatterniaHub/coreStaticStyles";
+import { ExtractGaladrielCSSClassesType } from "../../../types/coreTypes";
+import { computeConfigOnNested } from "./computeConfigOnNested";
+import { hashHex } from "./hashHex";
+
+const getClasses: ExtractGaladrielCSSClassesType = (classes) => classes;
+
+const getStaticStyles = (key: string, value: any): Record<string, string> | null => {
+    try {
+        const staticHandler = coreStaticStyles[key];
+
+        if (staticHandler) {
+            const propertiesHandler = staticHandler({
+                extractGaladrielClasses: getClasses,
+            });
+
+            const selector = `.${value}`;
+            const propertiesValues = propertiesHandler[selector];
+
+            if (propertiesValues) {
+                return propertiesValues;
+            }
+        }
+    } catch (error: any) {
+        console.error("An error occurred:", error);
+    }
+
+    return null;
+};
+
+const getDynamicStyles = (key: string): string | null => {
+    try {
+        return coreDynamicProperties[key];
+    } catch (error: any) {
+        console.error("An error occurred:", error);
+    }
+
+    return null;
+};
+
+export const getNestedStyles = (pseudo: string, node: Node) => {
+    const testRegex = /^\$\w+(-\w+)*$/;
+    const nestedClasses: string[] = [];
+
+    (node as any).properties.forEach((property: any) => {
+        if ((property as any).value.type.includes("Literal")) {
+            const key = (property as any).key.name;
+            const value = (property as any).value.value;
+
+            if (key && typeof key === "string") {
+                if ( value && typeof value === "string" && testRegex.test(value)) {
+                    const staticStyle = getStaticStyles(key, value);
+
+                    if (staticStyle) {
+                        const [[objKey, objValue]] = Object.entries(staticStyle);
+
+                        nestedClasses.push(`${objKey}:${objValue}`);
+                    } else {
+                        const customClassName = value.replace("$", "");
+                        const customStyle = computeConfigOnNested(customClassName);
+
+                        if (customStyle) {
+                            const { customKey, customValue } = customStyle;
+
+                            nestedClasses.push(`${customKey}:${customValue}`);
+                        }
+                    }
+                } else if (value && typeof value === "string") {
+                    const classContent = getDynamicStyles(key);
+
+                    if (classContent) {
+                        nestedClasses.push(`${classContent}:${value}`);
+                    }
+                }
+            }
+        }
+    });
+
+    if (nestedClasses) {
+        const hashedHex = hashHex(nestedClasses.join(" "));
+        const rules = nestedClasses.join(";");
+        const pseudoClass = coreDynamicProperties[pseudo];
+
+        if (pseudoClass.includes("$")) {
+            // replace the node value to the class name
+            (node as any).init = `galadriel_${hashedHex}`;
+
+            return `@media screen and (${pseudoClass.replace("$", "")}) { .galadriel_${hashedHex} { ${rules} } }`;
+        } else {
+            // replace the node value to the class name
+            (node as any).init = `galadriel_${pseudoClass.replace("&", hashedHex)}`;
+
+            return `.galadriel_${pseudoClass.replace("&", hashedHex)} { ${rules} }`;
+        }
+    }
+
+    return "";
+};
