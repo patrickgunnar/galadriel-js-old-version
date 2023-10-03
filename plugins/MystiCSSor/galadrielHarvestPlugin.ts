188eb6997f2757827be5001c660c18765005ee0f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-03 11:05:30 -0300

    refactor: removed unnecessary comments

--- plugins/MystiCSSor/galadrielHarvestPlugin.ts
+++ plugins/MystiCSSor/galadrielHarvestPlugin.ts
@@ -3,7 +3,6 @@ import { extractClasses } from "./scripts/extractClasses";
 import { parseConfig } from "./scripts/parseConfig";
 import path from "path";
 
-//const completedNodes: string[] = [];
 const styleClasses: string[] = [];
 
 export default function (): PluginObj {
@@ -21,12 +20,7 @@ export default function (): PluginObj {
                 if (!shouldExclude || shouldInclude) {
                     try {
                         const node = path.node as Node;
-                        //const stringifiedNode = JSON.stringify(node);
-                        //const hasCompletedNodes = completedNodes.join(" ");
-
-                        if (node) { // && !hasCompletedNodes.includes(stringifiedNode)
-                            //completedNodes.push(stringifiedNode);
-
+                        if (node) {
                             const classes = extractClasses(node);
 
                             if (classes && !styleClasses.includes(classes)) {
