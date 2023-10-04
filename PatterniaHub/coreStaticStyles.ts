ff1b4e599016eb5b50ed23c23928e44246b39f4d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-04 10:15:01 -0300

    feat: added footer elements styles

--- PatterniaHub/coreStaticStyles.ts
+++ PatterniaHub/coreStaticStyles.ts
@@ -2,9 +2,11 @@ import { CoreStaticStylesType } from "../types/coreTypes";
 import { CSSUtil } from "./StaticVault/CSSUtil";
 import { ButtonCSSUtil } from "./StaticVault/ButtonCSSUtil";
 import { NavbarCSSUtil } from "./StaticVault/NavbarCSSUtil";
+import { FooterCSSUtil } from "./StaticVault/FooterCSSUtil";
 
 export const coreStaticStyles: CoreStaticStylesType = {
     ...CSSUtil,
     ...ButtonCSSUtil,
     ...NavbarCSSUtil,
+    ...FooterCSSUtil,
 };
