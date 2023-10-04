07a7b6f64f29514bd4d1adfdaff3d2f808ecb049
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-03 21:57:26 -0300

    refactor: added navbar

--- PatterniaHub/coreStaticStyles.ts
+++ PatterniaHub/coreStaticStyles.ts
@@ -1,8 +1,10 @@
 import { CoreStaticStylesType } from "../types/coreTypes";
 import { CSSUtil } from "./StaticVault/CSSUtil";
 import { ButtonCSSUtil } from "./StaticVault/ButtonCSSUtil";
+import { NavbarCSSUtil } from "./StaticVault/NavbarCSSUtil";
 
 export const coreStaticStyles: CoreStaticStylesType = {
     ...CSSUtil,
     ...ButtonCSSUtil,
+    ...NavbarCSSUtil,
 };
