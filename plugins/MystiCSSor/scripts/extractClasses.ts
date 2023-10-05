1aadd9eb1f37be8dd2e51fc74fc2be08966c717c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-05 17:21:22 -0300

    refactor: added refine css cls from nodes

--- plugins/MystiCSSor/scripts/extractClasses.ts
+++ plugins/MystiCSSor/scripts/extractClasses.ts
@@ -1,26 +1,5 @@
 import { Node } from "@babel/types";
-import { getStyleClasses } from "./getStyles";
-import { getNestedStyles } from "./getNestedStyles";
-
-type ParsedValue = string | null;
-
-const extractNestedObjs = (key: string, node: Node): ParsedValue => {
-    try {
-        if ((node as any).type.includes("Literal")) {
-            const styles = getStyleClasses(key, node);
-
-            if(styles) return styles;
-        } else if (node.type === "ObjectExpression" && node.properties) {
-            const styles = getNestedStyles(key, node)
-
-            if (styles) return styles;
-        }
-    } catch (error: any) {
-        console.error("An error occurred:", error);
-    }
-
-    return null; // Handle unknown node types
-};
+import { refineCSSClassesFromNode } from "./refineCSSClassesFromNode";
 
 const extractClasses = (node: Node) => {
     if (node.type === "ObjectExpression" && node.properties) {
@@ -33,7 +12,7 @@ const extractClasses = (node: Node) => {
                     const key = (property.key as any).name;
 
                     if (key) {
-                        const classValues = extractNestedObjs(key, property.value);
+                        const classValues = refineCSSClassesFromNode(key, property.value);
 
                         if(classValues) styleClasses.push(classValues);
                     }
