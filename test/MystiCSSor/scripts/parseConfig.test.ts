9cca7152d78943654776b088109ac7cadfddcb0c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-07 20:14:37 -0300

    test: uncommented the error handling test

--- test/MystiCSSor/scripts/parseConfig.test.ts
+++ test/MystiCSSor/scripts/parseConfig.test.ts
@@ -1,11 +1,9 @@
 import { parseConfig } from "../../../plugins/MystiCSSor/scripts/parseConfig";
 
-/*
 jest.mock("path", () => ({
     ...jest.requireActual("path"),
     resolve: jest.fn(() => "nonexistent.config.js"), // Mocked resolve function
 }));
-*/
 
 describe("Verify if parseConfig function is parsing the galadriel.config .js or .ts", () => {
     test("Check if configContent exists", () => {
@@ -20,7 +18,6 @@ describe("Verify if parseConfig function is parsing the galadriel.config .js or 
         expect(typeof configContent).toBe("object");
     });
 
-    /*
     test("Check error handling when configuration file doesn't exist", () => {
         // Simulate an error when file doesn't exist
         jest.spyOn(console, "error").mockImplementation(() => {});
@@ -30,5 +27,4 @@ describe("Verify if parseConfig function is parsing the galadriel.config .js or 
         expect(console.error).toHaveBeenCalled();
         expect(configContent).toEqual([]);
     });
-    */
 });
