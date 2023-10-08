bb8ea90d95096135a7d4a68a2c0e838a9a3cbb42
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-08 19:44:46 -0300

    feat: exporting the craft styles functions

---
+++ main.ts
@@ -0,0 +1,4 @@
+import { craftingStyles } from "./StyleCrafters/craftingStyles";
+import { craftingStyledElement } from "./StyleCrafters/craftingStyledElement";
+
+export { craftingStyles, craftingStyledElement };
