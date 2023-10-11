5a027390a79af3fc8013a5ffb3fe65e312e0f412
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-11 19:17:50 -0300

    refactor: added the for loop inside condition

--- plugins/MystiCSSor/scripts/refineCSSClassesFromNode.ts
+++ plugins/MystiCSSor/scripts/refineCSSClassesFromNode.ts
@@ -10,18 +10,18 @@ const refineCSSClassesFromNode = (
     coreAST: Record<string, Record<string, string[]>>
 ): ParsedValue => {
     try {
-        let coreNodeName = "otherProperties";
-
-        for (const coreKey in coreAST) {
-            const currentCoreNode = coreAST[coreKey][key];
-
-            if (currentCoreNode && typeof currentCoreNode === "object") {
-                coreNodeName = coreKey;
-                break;
-            }
-        }
-
         if ((node as any).type.includes("Literal")) {
+            let coreNodeName = "otherProperties";
+
+            for (const coreKey in coreAST) {
+                const currentCoreNode = coreAST[coreKey][key];
+
+                if (currentCoreNode && typeof currentCoreNode === "object") {
+                    coreNodeName = coreKey;
+                    break;
+                }
+            }
+        
             const styles = retrieveStyleClasses(key, node, coreAST[coreNodeName]);
 
             if (styles && typeof styles === "string") return styles;
