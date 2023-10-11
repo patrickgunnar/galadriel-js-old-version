628025738b57b6f5144a8c375c82aa967a6fbdea
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-11 19:53:46 -0300

    feat: collects AST classes and return string

---
+++ plugins/MystiCSSor/AST/uniteGaladrielAST.ts
@@ -0,0 +1,28 @@
+import { coreDynamicProperties } from "../../../PatterniaHub/coreDynamicProperties";
+import { coreAST } from "./coreAST";
+
+const uniteGaladrielAST = (): string => {
+    let classesString = "";
+
+    for (const node in coreAST) {
+        if (coreAST[node]) {
+            for (const child in coreAST[node]) {
+                if (coreAST[node][child].length > 0) {
+                    if (node === "mediaQueryVariables") {
+                        const mediaValue = coreDynamicProperties[child].replace("$", "");
+
+                        if (mediaValue) {
+                            classesString += ` @media screen and (${mediaValue}) { ${coreAST[node][child].join(" ")} }`;
+                        }
+                    } else {
+                        classesString += ` ${coreAST[node][child].join(" ")}`;
+                    }
+                }
+            }
+        }
+    }
+
+    return classesString;
+};
+
+export { uniteGaladrielAST };
