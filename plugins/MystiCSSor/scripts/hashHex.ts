26f3365bddadb01ac191473c21c6ed4efbd7c1f4
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-13 17:33:07 -0300

    feat: added a condition to gen a 96bits code

--- plugins/MystiCSSor/scripts/hashHex.ts
+++ plugins/MystiCSSor/scripts/hashHex.ts
@@ -1,12 +1,15 @@
 import crypto from "crypto";
 
-const hashHex = (str: string) => {
+const hashHex = (str: string, is96bits=false) => {
     const hash = crypto.createHash("sha256");
     const updatedHash = hash.update(str);
     const digestedHash = updatedHash.digest("hex");
-    const hashedHex = digestedHash.substring(digestedHash.length - 8);
 
-    return hashedHex;
+    if (!is96bits) {
+        return digestedHash.substring(digestedHash.length - 8);
+    } else {
+        return digestedHash.substring(digestedHash.length - 12);
+    }
 };
 
 export { hashHex };
