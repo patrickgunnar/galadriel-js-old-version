b55e90aaba06d7ee030decf4aeab75d82134e8a3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-13 14:51:32 -0300

    refactor: omitted the export

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
 import { craftingStyles } from "./StyleCrafters/craftingStyles";
-import { craftingStyledElement } from "./StyleCrafters/craftingStyledElement";
 
-export { craftingStyles, craftingStyledElement };
+export { craftingStyles };
