083d4532949834f811f7f471d0995331f58af6ee
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-14 11:05:19 -0300

    feat: added node control

--- plugins/MystiCSSor/galadrielHarvestPlugin.ts
+++ plugins/MystiCSSor/galadrielHarvestPlugin.ts
@@ -3,6 +3,11 @@ import { PluginObj } from "@babel/core";
 import { parseConfig } from "./scripts/parseConfig";
 import { coreAST } from "./AST/coreAST";
 import { extractObjectsFromNode } from "./scripts/extractObjectsFromNode";
+import { hashHex } from "./scripts/hashHex";
+import generate from "@babel/generator";
+import { parse } from "@babel/parser";
+
+const modifiedCallExpressionNodes: Record<string, any> = {}
 
 /**
  * Exported default function to process a Babel plugin.
@@ -27,10 +32,39 @@ export default function ({ types }: { types: any }): PluginObj {
                     const callee = path.get("callee");
 
                     if (callee.isIdentifier({ name: "craftingStyles" })) {
-                        const callbackArgument = path.node.arguments[0];
+                        const hashedNode = hashHex(JSON.stringify(path.node), true);
+                        const modifiedNode = modifiedCallExpressionNodes[hashedNode];
 
-                        if (callbackArgument && (callbackArgument.type === 'ArrowFunctionExpression' || callbackArgument.type === 'FunctionExpression')) {
-                            extractObjectsFromNode(types, callbackArgument.body, coreAST)
+                        if (!modifiedNode) {
+                            const callbackArgument = path.node.arguments[0];
+
+                            if (callbackArgument && (callbackArgument.type === 'ArrowFunctionExpression' || callbackArgument.type === 'FunctionExpression')) {
+                                extractObjectsFromNode(types, callbackArgument.body, coreAST);
+
+                                const modifiedNodeCode = generate(path.node).code;
+
+                                if (modifiedNodeCode) {
+                                    const modifiedParsedCode = parse(modifiedNodeCode, { plugins: ["typescript"] });
+
+                                    if (modifiedParsedCode) {
+                                        const modifiedParsedBody = modifiedParsedCode.program.body[0];
+
+                                        if (modifiedParsedBody.type === "ExpressionStatement") {
+                                            const expression = modifiedParsedBody.expression;
+
+                                            if (expression) {
+                                                modifiedCallExpressionNodes[hashedNode] = expression;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            const callbackArgument = path.node.arguments[0];
+
+                            if (callbackArgument && (callbackArgument.type === 'ArrowFunctionExpression' || callbackArgument.type === 'FunctionExpression')) {
+                                callbackArgument.body = modifiedNode.arguments[0].body;
+                            }
                         }
                     }
                 }
