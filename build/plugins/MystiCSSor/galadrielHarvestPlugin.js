5987a29e3c3a3f4260dc6531c82692860a31349c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-15 10:22:30 -0300

    refactor: build content

--- build/plugins/MystiCSSor/galadrielHarvestPlugin.js
+++ build/plugins/MystiCSSor/galadrielHarvestPlugin.js
@@ -9,7 +9,9 @@ const coreAST_1 = require("./AST/coreAST");
 const extractObjectsFromNode_1 = require("./scripts/extractObjectsFromNode");
 const hashHex_1 = require("./scripts/hashHex");
 const lodash_1 = require("lodash");
-const modifiedCallExpressionNodes = {};
+const generator_1 = __importDefault(require("@babel/generator"));
+const transformedCallExpressionNodes = {};
+const transformedCSSRules = {};
 /**
  * Exported default function to process a Babel plugin.
  *
@@ -32,12 +34,13 @@ function default_1({ types }) {
                     if (callee.isIdentifier({ name: "craftingStyles" })) {
                         const callbackArgument = path.node.arguments[0];
                         if (callbackArgument && (callbackArgument.type === 'ArrowFunctionExpression' || callbackArgument.type === 'FunctionExpression')) {
-                            const hashedNode = (0, hashHex_1.hashHex)(JSON.stringify(path.node), true);
-                            const modifiedNode = modifiedCallExpressionNodes[hashedNode];
+                            const callbackBody = (0, generator_1.default)(callbackArgument.body).code.replace(/\s+/g, "");
+                            const hashedNode = (0, hashHex_1.hashHex)(JSON.stringify(callbackBody), true);
+                            const modifiedNode = transformedCallExpressionNodes[hashedNode];
                             if (!modifiedNode) {
-                                (0, extractObjectsFromNode_1.extractObjectsFromNode)(types, callbackArgument.body, coreAST_1.coreAST);
+                                (0, extractObjectsFromNode_1.extractObjectsFromNode)(types, callbackArgument.body, coreAST_1.coreAST, transformedCSSRules);
                                 const modifiedBodyClone = (0, lodash_1.cloneDeep)(callbackArgument.body);
-                                modifiedCallExpressionNodes[hashedNode] = modifiedBodyClone;
+                                transformedCallExpressionNodes[hashedNode] = modifiedBodyClone;
                             }
                             else {
                                 callbackArgument.body = (0, lodash_1.cloneDeep)(modifiedNode);
--- build/plugins/MystiCSSor/scripts/extractObjectsFromNode.js
+++ build/plugins/MystiCSSor/scripts/extractObjectsFromNode.js
@@ -1,32 +1,69 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.extractObjectsFromNode = void 0;
+const hashHex_1 = require("./hashHex");
 const quantumMesh_1 = require("./quantumMesh");
 const retrieveStylesInsideNestedObjects_1 = require("./retrieveStylesInsideNestedObjects");
 const retrieveStylesOutsideNestedObjects_1 = require("./retrieveStylesOutsideNestedObjects");
+const generator_1 = __importDefault(require("@babel/generator"));
 /**
  * Extract objects and process associated styles from a given node.
  *
  * @param {any} types - The types object for node analysis.
  * @param {any} node - The node to extract objects from.
  * @param {Record<string, Record<string, string[]>>} coreAST - The core AST to update with styles.
+ * @param {Record<string, Record<string, string>>} transformedCSSRules - The control of used CSS rules
  */
-function extractObjectsFromNode(types, node, coreAST) {
+function extractObjectsFromNode(types, node, coreAST, transformedCSSRules) {
     try {
         if (types.isObjectExpression(node)) {
             for (const prop of node.properties) {
                 let objectContent;
                 if (types.isIdentifier(prop.key)) {
+                    const transformedKey = transformedCSSRules[prop.key.name];
                     if (prop.value.type === "StringLiteral") {
+                        const transformedValueKey = (0, hashHex_1.hashHex)(prop.value.value, true);
+                        if (transformedKey) {
+                            const existingValue = transformedKey[transformedValueKey];
+                            if (existingValue) {
+                                prop.value.value = existingValue;
+                                continue;
+                            }
+                        }
                         objectContent = (0, retrieveStylesOutsideNestedObjects_1.retrieveStylesOutsideNestedObjects)(prop.key.name, prop.value.value);
                         if (objectContent) {
                             const { name } = objectContent;
                             if (name) {
                                 prop.value.value = name;
+                                if (transformedKey) {
+                                    transformedKey[transformedValueKey] = name;
+                                }
+                                else {
+                                    Object.assign(transformedCSSRules, {
+                                        [prop.key.name]: {
+                                            [transformedValueKey]: name,
+                                        },
+                                    });
+                                }
                             }
                         }
                     }
                     else if (prop.value.type === "ObjectExpression") {
+                        const genProps = prop.value.properties
+                            .map((item) => (0, generator_1.default)(item).code.replace(/\s+/g, ""))
+                            .join("");
+                        const transformedValueKey = (0, hashHex_1.hashHex)(genProps, true);
+                        if (transformedKey) {
+                            const existingValue = transformedKey[transformedValueKey];
+                            if (existingValue) {
+                                prop.value.type = "StringLiteral";
+                                prop.value.value = existingValue;
+                                continue;
+                            }
+                        }
                         const nestedProperties = [];
                         for (const nestedProp of prop.value.properties) {
                             if (nestedProp.value.type === "StringLiteral") {
@@ -44,6 +81,17 @@ function extractObjectsFromNode(types, node, coreAST) {
                                 if (name) {
                                     prop.value.type = "StringLiteral";
                                     prop.value.value = name;
+                                    if (transformedKey) {
+                                        transformedKey[transformedValueKey] =
+                                            name;
+                                    }
+                                    else {
+                                        Object.assign(transformedCSSRules, {
+                                            [prop.key.name]: {
+                                                [transformedValueKey]: name,
+                                            },
+                                        });
+                                    }
                                 }
                             }
                         }
@@ -75,7 +123,7 @@ function extractObjectsFromNode(types, node, coreAST) {
             if (node[key] &&
                 typeof node[key] === "object" &&
                 !(node.type === "ObjectExpression" && Array.isArray(node[key]))) {
-                extractObjectsFromNode(types, node[key], coreAST);
+                extractObjectsFromNode(types, node[key], coreAST, transformedCSSRules);
             }
         }
     }
