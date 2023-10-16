c7292a33469810c7e48456c824a8e054779a3086
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-16 19:19:21 -0300

    refactor: added comments

--- plugins/MystiCSSor/galadrielInjectionPlugin.ts
+++ plugins/MystiCSSor/galadrielInjectionPlugin.ts
@@ -1,17 +1,32 @@
 import postcss, { Result, Root } from "postcss";
 import { uniteGaladrielAST } from "./AST/uniteGaladrielAST";
 
+/**
+ * This module exports a PostCSS plugin function that processes the CSS AST and injects Galadriel CSS rules.
+ * @module galadrielInjectionPlugin
+ */
+
+/**
+ * The PostCSS plugin function.
+ * @function
+ * @returns {Object} The PostCSS plugin object.
+ */
 module.exports = function () {
     return {
+        // Define the plugin's name
         postcssPlugin: "galadrielInjectionPlugin",
+        // Define the processing logic for the plugin
         Once(root: Root, { result }: { result: Result }) {
             try {
+                // Unite the Galadriel AST into a string
                 const strFromAST = uniteGaladrielAST();
 
                 if (strFromAST) {
+                    // Parse the string into PostCSS rules
                     const parsedRules = postcss.parse(strFromAST);
 
                     if (parsedRules) {
+                        // Append the parsed rules to the root
                         root.append(parsedRules);
                     }
                 }
