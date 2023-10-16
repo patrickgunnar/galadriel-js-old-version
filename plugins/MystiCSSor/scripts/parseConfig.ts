7b17936642ec7191381ff2a5b348ee8820748e0b
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-16 14:09:50 -0300

    refactor: added comments

--- plugins/MystiCSSor/scripts/parseConfig.ts
+++ plugins/MystiCSSor/scripts/parseConfig.ts
@@ -1,13 +1,22 @@
 import path from "path";
 
+/**
+ * Parses the Galadriel configuration file (galadriel.config.ts or galadriel.config.js).
+ *
+ * @returns {object} The parsed configuration object.
+ */
 const parseConfig = () => {
+    // Paths for the Galadriel configuration files
     const configPaths = ["galadriel.config.ts", "galadriel.config.js"];
 
     try {
         for (const __path of configPaths) {
+            // Resolve the full path of the configuration file
             const fullPath = path.resolve(__path);
+            // Delete the cached module to ensure fresh require
             delete require.cache[require.resolve(fullPath)];
 
+            // Return the required configuration file if found
             return require(fullPath);
         }
     } catch (error: any) {
