18aac74297213e3d35bcfca18a652b58fb282b79
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-16 19:09:45 -0300

    refactor: added comments

--- plugins/MystiCSSor/scripts/retrieveStylesInsideNestedObjects.ts
+++ plugins/MystiCSSor/scripts/retrieveStylesInsideNestedObjects.ts
@@ -19,9 +19,11 @@ function retrieveStylesInsideNestedObjects(
     nestedValue: string
 ): string | null {
     if (key && nestedValue) {
+        // Check if nestedValue follows a specific pattern (starts with "$" followed by alphanumeric characters and dashes)
         const testRegex = /^\$\w+(-\w+)*$/;
 
         if (testRegex.test(nestedValue)) {
+            // If the nestedValue follows the pattern, it's a static or config value
             const property = genStaticAndConfigClasses(
                 nestedKey,
                 nestedValue,
@@ -29,25 +31,30 @@ function retrieveStylesInsideNestedObjects(
             );
 
             if (property && typeof property === "object") {
+                // If property is an object (static or config style), extract and format the CSS property and value
                 const [[propertyKey, propertyValue]] = Object.entries(property);
 
                 if (propertyKey && propertyValue) {
                     return `${propertyKey}:${propertyValue}`;
                 }
             } else if (typeof property === "string") {
+                // If property is a string (dynamic style), return it directly
                 return property;
             }
         } else {
+            // If the nestedValue doesn't follow the pattern, it's a dynamic value
             const property = genDynamicAndConfigClasses(
                 nestedKey,
                 nestedValue,
                 true
             );
 
+            // Return the processed CSS style for dynamic value
             return `${property}:${nestedValue}`;
         }
     }
 
+    // Return null if unable to generate
     return null;
 }
 
