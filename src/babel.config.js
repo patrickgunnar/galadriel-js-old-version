f979d58cee2ffd5a2eaf5f146241975f2f1cb6bf
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-30 20:11:33 -0300

    refactor: removed config

--- src/babel.config.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    presets: ["@babel/preset-env", "@babel/preset-typescript"],
-    plugins: [path.join(__dirname, "..", "build", "src", "plugins", "mystiCSSor", "galadrielHarvestPlugin")],
-};
