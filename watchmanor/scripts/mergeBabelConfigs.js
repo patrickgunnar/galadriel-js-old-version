b2f7ef53852e81d337d1ffe474dba749ed5ef4ea
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-30 10:04:31 -0300

    refactor: added import for .babelrc

--- watchmanor/scripts/mergeBabelConfigs.js
+++ watchmanor/scripts/mergeBabelConfigs.js
@@ -1,17 +1,21 @@
 const deepmerge = require("deepmerge");
-const babel = require("@babel/core");
 const fs = require("fs");
+const path = require("path");
 
 // Import the internal Babel configuration
-const internalBabelConfig = require("./babel.config.js");
+const internalBabelConfig = require("../../src/babel.config.js");
 
 // Look for user's Babel configuration
 const userBabelConfig = fs.existsSync("./babel.config.js")
-    ? require("./babel.config.js")
+    ? require(path.resolve("babel.config.js"))
     : fs.existsSync(".babelrc")
-    ? require(".babelrc")
+    ? JSON.parse(fs.readFileSync(path.resolve(".babelrc")))
     : null;
 
-function mergeBabelConfigs() {}
+function mergeBabelConfigs() {
+    return userBabelConfig
+        ? deepmerge(internalBabelConfig, userBabelConfig)
+        : internalBabelConfig;
+}
 
 module.exports = { mergeBabelConfigs };
