43df98c7c1eff5b981c8a327b5c66cb56fc5b401
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-29 22:03:44 -0300

    feat: logger class

---
+++ Watchmanor/scripts/logger.js
@@ -0,0 +1,25 @@
+class Logger {
+    #getCurrentTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+        const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
+
+        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    }
+
+    makeBold(msg) {
+        return `\x1b[1m${msg}\x1b[0m`;
+    }
+
+    now(message, bold = false) {
+        const now = bold
+            ? this.makeBold(this.#getCurrentTime())
+            : this.#getCurrentTime();
+
+        console.log(`[${now}]: ${message}`);
+    }
+}
+
+module.exports = { Logger };
