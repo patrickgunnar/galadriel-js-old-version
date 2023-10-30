d7302acec7576508bbdfbd825a7be4f47b3e3875
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-30 10:31:23 -0300

    fix: fixed plugin path

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,4 +1,6 @@
+const path = require("path");
+
 module.exports = {
     presets: ["@babel/preset-env", "@babel/preset-typescript"],
-    plugins: ["build/src/plugins/mystiCSSor/galadrielHarvestPlugin"],
+    plugins: [path.join(__dirname, "..", "build", "src", "plugins", "mystiCSSor", "galadrielHarvestPlugin")],
 };
