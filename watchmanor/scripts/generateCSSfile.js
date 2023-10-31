ab2237060c0ba76d2958cbcdbb780f16219dbd6c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-30 22:43:42 -0300

    refactor: changed file & handler's name

---
+++ watchmanor/scripts/generateCSSfile.js
@@ -0,0 +1,21 @@
+const babel = require("@babel/core");
+const path = require("path");
+const fs = require("fs");
+const babelConfig = require(path.join(__dirname, "..", "..", "src", "babel.internal.config.js"));
+const coreAST = require(path.join(__dirname, "..", "..", "build", "src", "ast", "uniteGaladrielAST.js"));
+
+function generateCSSfile(codeString, fileName, output) {
+    const transpiledCode = babel.transformSync(codeString, {
+        ...babelConfig,
+        filename: fileName,
+    });
+
+    if (transpiledCode) {
+        const contentCSS = coreAST.uniteGaladrielAST();
+        const __output = path.resolve(output);
+
+        fs.writeFileSync(__output, contentCSS);
+    }
+}
+
+module.exports = { generateCSSfile };
