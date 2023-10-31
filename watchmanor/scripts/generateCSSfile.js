22ee46d4e76179c48355be9a4ee2705cd28c1ae3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-10-30 23:05:17 -0300

    refactor: improved css handler import

--- watchmanor/scripts/generateCSSfile.js
+++ watchmanor/scripts/generateCSSfile.js
@@ -2,7 +2,7 @@ const babel = require("@babel/core");
 const path = require("path");
 const fs = require("fs");
 const babelConfig = require(path.join(__dirname, "..", "..", "src", "babel.internal.config.js"));
-const coreAST = require(path.join(__dirname, "..", "..", "build", "src", "ast", "uniteGaladrielAST.js"));
+const { uniteGaladrielAST } = require(path.join(__dirname, "..", "..", "build", "src", "ast", "uniteGaladrielAST.js"));
 
 function generateCSSfile(codeString, fileName, output) {
     const transpiledCode = babel.transformSync(codeString, {
@@ -11,7 +11,7 @@ function generateCSSfile(codeString, fileName, output) {
     });
 
     if (transpiledCode) {
-        const contentCSS = coreAST.uniteGaladrielAST();
+        const contentCSS = uniteGaladrielAST();
         const __output = path.resolve(output);
 
         fs.writeFileSync(__output, contentCSS);
